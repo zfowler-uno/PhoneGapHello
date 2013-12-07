6050e0f84ce89d2fffb737ef33858762c0a29f4d
Zac Fowler <zfowler@unomaha.edu>
2013-12-06 22:27:40 -0600

    More changes. Last ditch effort to try a new upload.

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\Users\Zac\Documents\GitHub\PhoneGapHello\platforms\android\gen\com\phonegap\helloworld\R.java \
+ : C:\Users\Zac\Documents\GitHub\PhoneGapHello\platforms\android\res\drawable\icon.png \
+C:\Users\Zac\Documents\GitHub\PhoneGapHello\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\Zac\Documents\GitHub\PhoneGapHello\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\Zac\Documents\GitHub\PhoneGapHello\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\Zac\Documents\GitHub\PhoneGapHello\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\Zac\Documents\GitHub\PhoneGapHello\platforms\android\res\values\strings.xml \
+C:\Users\Zac\Documents\GitHub\PhoneGapHello\platforms\android\res\xml\config.xml \
+C:\Users\Zac\Documents\GitHub\PhoneGapHello\platforms\android\bin\AndroidManifest.xml \
